0558c6a4645a872a543dbc341ac873fa1943a261
Mitch de Peffer <mr_poffertje@hotmail.com>
2020-09-12 20:34:30 +0200

    Added more util functions

--- src/app.js
+++ src/app.js
@@ -9,8 +9,10 @@ import Vector from './core/utilities/Vector'
 import Ship from './game/Ship'
 
 import Color from './core/utilities/Color'
+import Utils from './core/utilities/Utils'
 
 window.color = Color
+window.utils = Utils
 
 const core = new Core(document.querySelector('#gamewindow'), 600, 400)
 const scene = new Scene(core)
--- src/core/utilities/Utils.js
+++ src/core/utilities/Utils.js
@@ -13,10 +13,29 @@ export default class Utils {
     return radians * (180 / Math.PI)
   }
 
-  static lerp (min = 0, max = 1, amount = 1) {
+  static lerp (amount = 1, min = 0, max = 1) {
     amount = Utils.clamp(amount, 0, 1)
     if (max < min) max = min
 
     return (1 - amount) * min + amount * max
+  }
+
+  static map (val, minOld, maxOld, minNew, maxNew) {
+    const mappedVal = (val - minOld) / (maxOld - minOld) * (maxNew - minNew) + minNew
+    return mappedVal
+  }
+
+  static random (maxOrMin = 1, max) {
+    const min = max ? maxOrMin : 0
+    max = max ? max : maxOrMin
+    if (max < min) max = min
+    
+    const deltaBounds = max - min
+
+    return min + Math.random() * deltaBounds
+  }
+
+  static randomInt(maxOrMin = 1, max) {
+    return Math.round(Utils.random(maxOrMin, max))
   }
 }
